6c132d436ff98bc84486547ea1bc8766e96c7815
Pujan Bhuva <pbhuva@timelessmedical.com>
2021-07-04 23:01:52 -0300

    singlePost and Project pages done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -9,7 +9,7 @@ export default function NavBar(){
             <div className="container mx-auto flex justify-between">
                 <nav className="flex">
                     <NavLink to="/" exact activeClassName="text-white" className="inline-flex items-center py-6 px-3 mr-4 hover:text-yellow-400 text-4xl font-bold cursive tracking-widest">
-                        Heinsenbug
+                        Heisenbug
                     </NavLink>
                     <NavLink to="/post" className="inline-flex items-center py-3 px-3 my-6 rounded text-white hover:text-yellow-400" activeClassName="text-yellow-100 bg-yellow-600">
                         Blog Posts
@@ -26,6 +26,7 @@ export default function NavBar(){
                     <SocialIcon url="https://www.instagram.com/theheisenbug/" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}} />
                     <SocialIcon url="https://www.linkedin.com/in/pujan-bhuva-8b4ab5111/" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}} />
                     <SocialIcon url="https://github.com/pujanb96" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}} />
+                    <SocialIcon url="https://www.facebook.com/pujanb96/" className="mr-4" target="_blank" fgColor="#fff" style={{ height: 35, width: 35}} />
                     
                 </div>
             </div>
